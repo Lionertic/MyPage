9dcebff735ab45f1d2aac22b4ec4ee39be3ff6a7
Uday <ucharan047@gmail.com>
2019-08-03 10:48:07 +0530

    animation
    Signed-off-by: Uday <ucharan047@gmail.com>

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,70 +1,52 @@
-$(document).ready(function(){
-  $('#img2').one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function() {
+$(() => {
+
+  const w = $(window).width();
+  const h = $(window).height();
+
+  $("#img2").one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function () {
     $(this).css("-webkit-animation-name", "img21d");
     $(this).css("-webkit-animation-iteration-count", "infinite");
     $(this).css("animation-name", "img21d");
     $(this).css("animation-iteration-count", "infinite");
   });
-  var w = $(window).width();
-  var h =$(window).height();
-  $(window).resize(function(){
-    var w = $(window).width();
-    var h =$(window).height();
+
+
+  $(window).resize(() => {
+    let w = $(window).width();
+    let h = $(window).height();
     if ((w<h)||(w<880 || h < 470)){
-      $("#img1").css("visibility","hidden");
-      $("#img2").css("visibility","hidden");
-      $("#img3").css("visibility","hidden");
-      $("#screen").css("visibility","visible");
-      $("body").css("overflow","hidden");
+      changeVisibility("hidden", "visible", "hidden")
     } else{
-      $("#screen").css("visibility","hidden");
-      $("#img1").css("visibility","visible");
-      $("#img2").css("visibility","visible");
-      $("#img3").css("visibility","visible");
-      $("body").css("overflow","auto");
+      changeVisibility("visible", "hidden", "auto");
     }
-});
-if (!detectmob()) {
-  if ((w<h)||(w<880 || h < 470)){
-    $("#img1").css("visibility","hidden");
-    $("#img2").css("visibility","hidden");
-    $("#img3").css("visibility","hidden");
-    $("#screen").css("visibility","visible");
-    $("body").css("overflow","hidden");
-  }
-  else{
-    $("#screen").css("visibility","hidden");
-    $("#img1").css("visibility","visible");
-    $("#img2").css("visibility","visible");
-    $("#img3").css("visibility","visible");
-    $("body").css("overflow","auto");
-    var image = document.createElement('img');
-    var i=0;
-    for(i=1;i<=3;i++){
-      var name="img";
+  });
+  if (!detectmob()) {
+    changeVisibility("visible", "hidden", "auto");
+    const image = document.createElement('img');
+
+    for (let i = 1; i <= 3; i++) {
+      const name = "img";
       image.src = getBgUrl(document.getElementById(name+i));
       image.onload = function () {
-        if(i==3){
+        if (i === 3) {
             setTimeout(showPage,500);
         }
       }
     }
-  }
-
-    var s = skrollr.init({
+    const s = skrollr.init({
       render: function(data) {
-        console.log($("#overlay").css("opacity"));
-        if (($("#overlay").css("opacity") > 0.8)||($("#overlay").css("opacity") == 0)) {
-
+        let opacity = $("#overlay").css("opacity");
+        if ((opacity > 0.8) || (opacity === 0)) {
           window.location.replace("desk.html#home");
         }
       }
     });
-} else {
+  } else {
     window.location.replace("mob.html");
-}
+  }
 });
-function detectmob() {
+
+const detectmob = () => {
   if (navigator.userAgent.match(/Android/i) ||
     navigator.userAgent.match(/webOS/i) ||
     navigator.userAgent.match(/iPhone/i) ||
@@ -75,20 +57,28 @@ function detectmob() {
     return true;
   else
     return false;
-}
+};
 
+const getBgUrl = (el) => {
+  let bg = "";
+  if (el.currentStyle) { // IE
+    bg = el.currentStyle.backgroundImage;
+  } else if (document.defaultView && document.defaultView.getComputedStyle) { // Firefox
+    bg = document.defaultView.getComputedStyle(el, "").backgroundImage;
+  } else { // try and get inline style
+    bg = el.style.backgroundImage;
+  }
+  return bg.replace(/url\(['"]?(.*?)['"]?\)/i, "$1");
+};
 
-function getBgUrl(el) {
-var bg = "";
-if (el.currentStyle) { // IE
-  bg = el.currentStyle.backgroundImage;
-} else if (document.defaultView && document.defaultView.getComputedStyle) { // Firefox
-  bg = document.defaultView.getComputedStyle(el, "").backgroundImage;
-} else { // try and get inline style
-  bg = el.style.backgroundImage;
-}
-return bg.replace(/url\(['"]?(.*?)['"]?\)/i, "$1");
-}
-function showPage() {
-  // document.body.classList.remove('js-loading');
-}
+const changeVisibility = (imgVisibility, screenVisibility, bodyOverflow) => {
+  $('#screen').css("visibility", screenVisibility);
+  $("#img1").css("visibility", imgVisibility);
+  $("#img2").css("visibility", imgVisibility);
+  $("#img3").css("visibility", imgVisibility);
+  $("body").css("overflow", bodyOverflow);
+};
+
+const showPage = () => {
+  document.body.classList.remove('js-loading');
+};
